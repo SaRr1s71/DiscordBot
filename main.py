aaed1d70293320109c6a501d785692886fbc87e4
SaRr1s71 <konstantakis2012@gmail.com>
2026-05-26 18:40:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import logging
 
 # 1. ΒΑΛΕ ΤΟ ΝΕΟ ΣΟΥ TOKEN ΕΔΩ (ΜΕΤΑ ΤΟ RESET)
-token = "MTUwODc4MDEyMjMwNjUxMDk1OQ.GTQ3mO.3kdFP-f-PHw2xvtMTQO3XMnSJyE7yHfX1CCY2k"
+token = "MTUwODc4MDEyMjMwNjUxMDk1OQ.G8iuir.KVF-M3iAvdqurgrFAz9q_eX5qS4cShxBH8Vuyc"
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
